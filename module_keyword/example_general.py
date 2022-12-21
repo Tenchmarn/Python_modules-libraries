ba2e56935fd512f91bf99ceae188bbb50104441f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2022-12-21 12:40:26 +0300

    Add files via upload

---
+++ module_keyword/example_general.py
@@ -0,0 +1,3 @@
+import keyword ## импорт модуля keyword
+print(keyword.kwlist) ## метод вывода списка всех ключевых слов
+print(keyword.iskeyword(None)) ## метод вывода, "является ли строка ключевым словом"
